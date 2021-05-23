18cbf13831304766a40b68eb63bc68bade8e7132
Giro1230 <hellowin1230@gmail.com>
2021-05-24 03:24:12 +0900

    learn_C++(Hello_World)

---
+++ HelloWorld.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+int main() {
+	int num = 20;
+	//std::cout<< '출력대상'
+	std::cout << "Hello World" << std::endl;
+	std::cout << "Hello" << "World" << std::endl;
+	std::cout << num << ' ' << 'A';
+	std::cout << ' ' << 3.14 << std::endl;
+	return 0;
+}
